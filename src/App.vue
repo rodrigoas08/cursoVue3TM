63b741b758f17d6134790f815127cf9580822243
Rodrigo Sobral <rodrigo.sobral@usabit.com.br>
2023-02-27 12:37:53 -0300

    aula 5: loopings usando v-for

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,68 @@
 <template>
-	<TheHeader v-if="showHeader" />
-	<div v-show="showName">
-		Nome: {{ firstName }}
-		<br />
-		Sobrenome: {{ lastName }}
+	<div>
+		<div v-for="(obj, index) in todos" :key="obj.id" class="todo-item">
+			{{ index + 1 }} - {{ obj.title }}
+		</div>
 	</div>
-
-	<div v-if="acessLevel === 'admin'">Usuário: Admin</div>
-	<div v-else-if="acessLevel === 'marketing'">Usuário: marketing</div>
-	<div v-else>Usuário: Visitante</div>
-  
-	<img alt="Vue logo" src="./assets/logo.png" />
-	<HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-import TheHeader from "./components/TheHeader.vue";
-
 export default {
 	name: "App",
-	components: {
-		HelloWorld,
-		TheHeader,
-	},
 	//data é a central de variáveis do componente, tudo que declarar aqui ficará disponível pra usar no componente
 	data() {
 		return {
-			showHeader: true,
-			firstName: "Jon",
-			lastName: "Snow",
-			showName: false,
-			acessLevel: "",
+			todos: [
+				{
+					userId: 1,
+					id: 1,
+					title: "delectus aut autem",
+					completed: false,
+				},
+				{
+					userId: 1,
+					id: 2,
+					title: "quis ut nam facilis et officia qui",
+					completed: false,
+				},
+				{
+					userId: 1,
+					id: 3,
+					title: "fugiat veniam minus",
+					completed: false,
+				},
+				{
+					userId: 1,
+					id: 4,
+					title: "et porro tempora",
+					completed: true,
+				},
+				{
+					userId: 1,
+					id: 5,
+					title:
+						"laboriosam mollitia et enim quasi adipisci quia provident illum",
+					completed: false,
+				},
+			],
 		};
 	},
 };
 </script>
 
 <style>
+.todo-item {
+	background: #00ff00;
+	color: #3d1365;
+	font-weight: bold;
+	margin: 0 0 3px 0;
+	padding: 3px 6px;
+}
 #app {
 	font-family: Avenir, Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
 	color: #2c3e50;
-	margin-top: 60px;
+	margin: 60px;
 }
 </style>
