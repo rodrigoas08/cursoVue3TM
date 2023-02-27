4a2f697fbf412da97e0d84f0437173eb53c5cb72
Rodrigo Sobral <rodrigo.sobral@usabit.com.br>
2023-02-27 13:23:40 -0300

    aula 5: não está na aula mas quis testar o todo usando axios

--- src/App.vue
+++ src/App.vue
@@ -7,45 +7,63 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
 	name: "App",
 	//data é a central de variáveis do componente, tudo que declarar aqui ficará disponível pra usar no componente
+	// data() {
+	// 	return {
+	// 		todos: [
+	// 			{
+	// 				userId: 1,
+	// 				id: 1,
+	// 				title: "delectus aut autem",
+	// 				completed: false,
+	// 			},
+	// 			{
+	// 				userId: 1,
+	// 				id: 2,
+	// 				title: "quis ut nam facilis et officia qui",
+	// 				completed: false,
+	// 			},
+	// 			{
+	// 				userId: 1,
+	// 				id: 3,
+	// 				title: "fugiat veniam minus",
+	// 				completed: false,
+	// 			},
+	// 			{
+	// 				userId: 1,
+	// 				id: 4,
+	// 				title: "et porro tempora",
+	// 				completed: true,
+	// 			},
+	// 			{
+	// 				userId: 1,
+	// 				id: 5,
+	// 				title:
+	// 					"laboriosam mollitia et enim quasi adipisci quia provident illum",
+	// 				completed: false,
+	// 			},
+	// 		],
+	// 	};
+	// },
+
 	data() {
 		return {
-			todos: [
-				{
-					userId: 1,
-					id: 1,
-					title: "delectus aut autem",
-					completed: false,
-				},
-				{
-					userId: 1,
-					id: 2,
-					title: "quis ut nam facilis et officia qui",
-					completed: false,
-				},
-				{
-					userId: 1,
-					id: 3,
-					title: "fugiat veniam minus",
-					completed: false,
-				},
-				{
-					userId: 1,
-					id: 4,
-					title: "et porro tempora",
-					completed: true,
-				},
-				{
-					userId: 1,
-					id: 5,
-					title:
-						"laboriosam mollitia et enim quasi adipisci quia provident illum",
-					completed: false,
-				},
-			],
+			todos: null,
 		};
+	},
+
+	mounted() {
+		axios
+			.get("https://jsonplaceholder.typicode.com/todos")
+			.then((response) => {
+				this.todos = response.data;
+			})
+			.catch((error) => {
+				console.error(error);
+			});
 	},
 };
 </script>
