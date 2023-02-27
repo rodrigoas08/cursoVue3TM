3152efd7859b8290ee96058473544c8a94b4327b
Rodrigo Sobral <rodrigo.sobral@usabit.com.br>
2023-02-27 11:03:21 -0300

    aula 3: criando componente e importando

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,14 @@
+<template>
+	<header class="header">Header</header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.header {
+	background: #000;
+	color: #fff;
+}
+</style>
