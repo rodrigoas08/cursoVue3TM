5fd9db5ecaf58876a9698c455c9d72e3018f88b5
Rodrigo Sobral <rodrigo.sobral@usabit.com.br>
2023-02-27 17:13:28 -0300

    aula 7: atributos Class e Styles dinâmicos

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,27 @@
 <template>
 	<div>
+		<h1 :class="{ title: true, 'title-home': isHome }">Curso Vue 3</h1>
+		<p :class="pClass">
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid eaque
+			voluptatum quis minima pariatur amet asperiores, impedit magnam ab quidem
+			obcaecati! Sed alias maiores repudiandae cum aperiam. Ipsam, obcaecati
+			doloremque?
+		</p>
+		<p :style="styleClass">
+			Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis nihil,
+			culpa quae consequuntur nam ex odit exercitationem sed ullam cupiditate
+			velit sapiente consequatur officia vero enim similique id consectetur ad?
+		</p>
 		<!-- v-bind pode ser substituido pelo ":". Ex: v-bind:key = :key -->
 		<div v-for="(obj, index) in todos" :key="obj.id" class="todo-item">
 			{{ index + 1 }} - {{ obj.title }}
 			<!-- esta img só irá exibir caso obj.imgSrc for true -->
-			<img class="todo-img" v-if="obj.imgSrc" :src="obj.imgSrc" :alt="obj.imgAlt" />
+			<img
+				class="todo-img"
+				v-if="obj.imgSrc"
+				:src="obj.imgSrc"
+				:alt="obj.imgAlt"
+			/>
 		</div>
 	</div>
 </template>
@@ -15,6 +32,10 @@ export default {
 	//data é a central de variáveis do componente, tudo que declarar aqui ficará disponível pra usar no componente
 	data() {
 		return {
+			isHome: true,
+			classVar: "",
+			pClass: ["text", { "text-home": this.isHome }],
+			styleClass: { color: "aqua", background: "black" },
 			todos: [
 				{
 					userId: 1,
@@ -58,6 +79,20 @@ export default {
 </script>
 
 <style>
+.title {
+	font-size: 20px;
+	color: blue;
+}
+.title-home {
+	font-size: 40px;
+	color: green;
+}
+.text {
+	color: gray;
+}
+.text-home {
+	color: greenyellow;
+}
 .todo-img {
 	display: block;
 }
